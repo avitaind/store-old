06ae153a1e3fbe520f5ac849dc15d1f8b30c7f6f
root <root@ip-172-31-34-21.us-east-2.compute.internal>
2019-10-31 07:29:15 +0000

    first commit

---
+++ app/design/frontend/M2themes/m2theme_child/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © 2016 M2themes. All rights reserved.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/M2themes/m2themes_child',
+    __DIR__
+);
---
+++ app/etc/env.php
@@ -0,0 +1,59 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin123',
+  ),
+  'crypt' => 
+  array (
+    'key' => '4e7ac70dab371f319f1a02bb43e7329c',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'shop',
+        'username' => 'avita',
+        'password' => 'AvitaBrandStore@123',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'developer',
+  'cache_types' => 
+  array (
+    'config' => 0,
+    'layout' => 0,
+    'block_html' => 0,
+    'collections' => 0,
+    'reflection' => 0,
+    'db_ddl' => 0,
+    'eav' => 0,
+    'customer_notification' => 0,
+    'full_page' => 0,
+    'config_integration' => 0,
+    'config_integration_api' => 0,
+    'translate' => 0,
+    'config_webservice' => 0,
+  ),
+  'install' => 
+  array (
+    'date' => 'Tue, 25 Jun 2019 05:03:24 +0000',
+  ),
+);
